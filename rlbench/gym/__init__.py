274950be4a74e2df3c626b217dff9338ba0e6e85
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-08-17 15:03:30 +0900

    add reach_target-vision-v1 env

--- rlbench/gym/__init__.py
+++ rlbench/gym/__init__.py
@@ -70,3 +70,13 @@ register(
         'action_mode': 'ABS_EE_XYZ_VELOCITY'
     }
 )
+
+# TEMP:
+register(
+    id='reach_target-vision-v1',
+    entry_point='rlbench.gym:RLBenchEnv',
+    kwargs={
+        'task_class': name_to_task_class('reach_target'),
+        'observation_mode': 'vision-front',
+    }
+)
